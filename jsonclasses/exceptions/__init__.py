87f671726f62e68dccd834b86f650ab3d855d5a4
Victor Zhang <yeannylam@gmail.com>
2020-09-04 11:26:06 +0800

    inline doc for exceptions

--- jsonclasses/exceptions/__init__.py
+++ jsonclasses/exceptions/__init__.py
@@ -1,7 +1,10 @@
 from typing import List, Dict, Any
 
 class ObjectNotFoundException(Exception):
-
+  '''ObjectNotFoundException is designed to be raised by jsonclasses ORM
+  integration implementations. Server authors and jsonclasses server integration
+  authors should catch this to present error to frontend clients.
+  '''
   def __init__(self, message: str):
     self.message = message
     super().__init__(self.message)
@@ -12,9 +15,10 @@ class UniqueFieldException(Exception):
     super().__init__(self.message)
 
 class ValidationException(Exception):
-
-  keypath_messages: Dict[str, str] = {}
-  root: Any
+  '''ValidationException is throwed on jsonclass object validation or on eager
+  validation. Server authors and jsonclasses server integration authors should
+  catch this to present error to frontend clients.
+  '''
 
   def __init__(self, keypath_messages: Dict[str, str], root: Any):
     self.keypath_messages = keypath_messages
