922a098b246927c37e84ba0bb23da7341c2eea42
Victor Zhang <yeannylam@gmail.com>
2020-09-06 16:57:22 +0800

    dynamic resolve validator class

--- jsonclasses/utils/default_validator_for_type.py
+++ jsonclasses/utils/default_validator_for_type.py
@@ -1,11 +1,5 @@
 from datetime import date, datetime
-from ..validators.chained_validator import ChainedValidator
-from ..validators.str_validator import StrValidator
-from ..validators.float_validator import FloatValidator
-from ..validators.int_validator import IntValidator
-from ..validators.bool_validator import BoolValidator
-from ..validators.date_validator import DateValidator
-from ..validators.datetime_validator import DatetimeValidator
+from .reference_map import resolve_class
 
 def default_validator_for_type(type):
   '''This function returns the default synthesized validator for given Python
@@ -18,17 +12,24 @@ def default_validator_for_type(type):
     Validator: A synthesized validator for the given Python type or None if it
     can't be synthesized.
   '''
+  ChainedValidator = resolve_class('ChainedValidator')
   if type is str:
+    StrValidator = resolve_class('StrValidator')
     return ChainedValidator().append(StrValidator())
   elif type is float:
+    FloatValidator = resolve_class('FloatValidator')
     return ChainedValidator().append(FloatValidator())
   elif type is int:
+    IntValidator = resolve_class('IntValidator')
     return ChainedValidator().append(IntValidator())
   elif type is bool:
+    BoolValidator = resolve_class('BoolValidator')
     return ChainedValidator().append(BoolValidator())
   elif type is date:
+    DateValidator = resolve_class('DateValidator')
     return ChainedValidator().append(DateValidator())
   elif type is datetime:
+    DatetimeValidator = resolve_class('DatetimeValidator')
     return ChainedValidator().append(DatetimeValidator())
   else:
     return None
