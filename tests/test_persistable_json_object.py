70a36b44790af1ed5587eabea25098d17fc08548
Victor Zhang <yeannylam@gmail.com>
2020-09-06 13:36:33 +0800

    persistable json object test

--- tests/test_persistable_json_object.py
+++ tests/test_persistable_json_object.py
@@ -1,1 +1,19 @@
-from jsonclasses import JSONObject
+import unittest
+from jsonclasses import jsonclass, PersistableJSONObject
+from datetime import datetime
+
+class TestPersistableJSONObject(unittest.TestCase):
+
+  def test_persistable_json_object_has_created_at_on_initializing(self):
+    o = PersistableJSONObject()
+    self.assertTrue(type(o.created_at) is datetime)
+    self.assertTrue(o.created_at < datetime.now())
+
+  def test_persistable_json_object_has_updated_at_on_initializing(self):
+    o = PersistableJSONObject()
+    self.assertTrue(type(o.updated_at) is datetime)
+    self.assertTrue(o.updated_at < datetime.now())
+
+  def test_persistable_json_object_has_id_with_default_value_none(self):
+    o = PersistableJSONObject()
+    self.assertTrue(o.id is None)
