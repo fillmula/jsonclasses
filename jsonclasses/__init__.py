a3ea53e89801905b309ed0f2bd878779b4f5a085
Victor Zhang <yeannylam@gmail.com>
2020-09-09 10:35:10 +0800

    api naming

--- jsonclasses/__init__.py
+++ jsonclasses/__init__.py
@@ -1,5 +1,10 @@
 from .jsonclass import jsonclass
-from .types import types
+from .types import types, Types
 from .json_object import JSONObject
 from .persistable_json_object import PersistableJSONObject
 from .json_encoder import JSONEncoder
+from .exceptions import ObjectNotFoundException, UniqueFieldException, ValidationException
+from .config import Config
+from .field import Field
+from .field_description import FieldDescription
+from .fields import fields, collection_argument_type_to_types
--- jsonclasses/validators/instanceof_validator.py
+++ jsonclasses/validators/instanceof_validator.py
@@ -5,12 +5,10 @@ from ..config import Config
 from ..graph import get_registered_class
 from ..exceptions import ValidationException
 from .validator import Validator
-from dataclasses import fields
-from ..fields import fields as our_fields
 from inflection import underscore, camelize
 from ..utils.keypath import keypath
 from ..reference_map import referenced, resolve_class
-from ..fields import collection_argument_type_to_types, fields as our_fields, dataclass_field_to_types
+from ..fields import collection_argument_type_to_types, fields, dataclass_field_to_types
 from ..field_description import WriteRule, ReadRule
 
 @referenced
@@ -34,10 +32,10 @@ class InstanceOfValidator(Validator):
     if hasattr(self, 'json_object_class_name') and self.json_object_class is None:
       self.json_object_class = get_registered_class(name=self.json_object_class_name, sibling=config.linked_class)
     keypath_messages = {}
-    for our_field in our_fields(value):
-      if our_field.field_types:
-        field_types = our_field.field_types
-        field_name = our_field.field_name
+    for field in fields(value):
+      if field.field_types:
+        field_types = field.field_types
+        field_name = field.field_name
         field_value = getattr(value, field_name)
         try:
           field_types.validator.validate(field_value, keypath(key_path, field_name), root, all_fields, config)
@@ -63,40 +61,40 @@ class InstanceOfValidator(Validator):
       if field.assigned_default_value is not None:
         setattr(base, field.field_name, field.assigned_default_value)
       else:
-        transformed_field_value = our_field.field_types.validator.transform(None, keypath(key_path, field.field_name), root, all_fields, config)
-        setattr(base, our_field.field_name, transformed_field_value)
-    for our_field in our_fields(base):
-      if our_field.json_field_name in value.keys() or our_field.field_name in value.keys():
-        field_value = value.get(our_field.json_field_name)
+        transformed_field_value = field.field_types.validator.transform(None, keypath(key_path, field.field_name), root, all_fields, config)
+        setattr(base, field.field_name, transformed_field_value)
+    for field in fields(base):
+      if field.json_field_name in value.keys() or field.field_name in value.keys():
+        field_value = value.get(field.json_field_name)
         if field_value is None and config.camelize_json_keys:
-          field_value = value.get(our_field.field_name)
-        if our_field.field_types.field_description.write_rule == WriteRule.NO_WRITE:
+          field_value = value.get(field.field_name)
+        if field.field_types.field_description.write_rule == WriteRule.NO_WRITE:
           if fill_blanks:
-            fill_blank_with_default_value(our_field)
-        elif our_field.field_types.field_description.write_rule == WriteRule.WRITE_ONCE:
-          current_field_value = getattr(base, our_field.field_name)
+            fill_blank_with_default_value(field)
+        elif field.field_types.field_description.write_rule == WriteRule.WRITE_ONCE:
+          current_field_value = getattr(base, field.field_name)
           if current_field_value is None or isinstance(current_field_value, Types):
-            transformed_field_value = our_field.field_types.validator.transform(field_value, keypath(key_path, our_field.field_name), root, all_fields, config)
-            setattr(base, our_field.field_name, transformed_field_value)
+            transformed_field_value = field.field_types.validator.transform(field_value, keypath(key_path, field.field_name), root, all_fields, config)
+            setattr(base, field.field_name, transformed_field_value)
           else:
             if fill_blanks:
-              fill_blank_with_default_value(our_field)
+              fill_blank_with_default_value(field)
         else:
-          transformed_field_value = our_field.field_types.validator.transform(field_value, keypath(key_path, our_field.field_name), root, all_fields, config)
-          setattr(base, our_field.field_name, transformed_field_value)
+          transformed_field_value = field.field_types.validator.transform(field_value, keypath(key_path, field.field_name), root, all_fields, config)
+          setattr(base, field.field_name, transformed_field_value)
       else:
         if fill_blanks:
-          fill_blank_with_default_value(our_field)
+          fill_blank_with_default_value(field)
     return base
 
   def tojson(self, value, config: Config, ignore_writeonly: bool = False):
     if value is None:
       return None
     retval = {}
-    for our_field in our_fields(value):
-      field_value = getattr(value, our_field.field_name)
-      json_field_name = our_field.json_field_name
-      if our_field.field_types.field_description.read_rule == ReadRule.NO_READ and not ignore_writeonly:
+    for field in fields(value):
+      field_value = getattr(value, field.field_name)
+      json_field_name = field.json_field_name
+      if field.field_types.field_description.read_rule == ReadRule.NO_READ and not ignore_writeonly:
         continue
-      retval[json_field_name] = our_field.field_types.validator.tojson(field_value, config)
+      retval[json_field_name] = field.field_types.validator.tojson(field_value, config)
     return retval
