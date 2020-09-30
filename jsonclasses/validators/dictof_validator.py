5c8dbb09a045cfd1ec3f547355c5c3c5954f88c9
Victor Zhang <yeannylam@gmail.com>
2020-09-30 11:58:08 +0800

    better dictof

--- jsonclasses/validators/dictof_validator.py
+++ jsonclasses/validators/dictof_validator.py
@@ -27,32 +27,24 @@ class DictOfValidator(TypeValidator):
             return
         super().validate(context)
         types = resolve_types(self.types, context.config.linked_class)
-        if types:
-            if types.field_description.item_nullability == Nullability.UNDEFINED:
-                types = types.required
-            keypath_messages = {}
-            for k, v in context.value.items():
-                try:
-                    item_context = ValidatingContext(
-                        value=v,
-                        keypath=concat_keypath(context.keypath, k),
-                        root=context.root,
-                        config=context.config,
-                        keypath_owner=concat_keypath(context.keypath_owner, k),
-                        owner=context.owner,
-                        config_owner=context.config_owner,
-                        keypath_parent=k,
-                        parent=context.value,
-                        field_description=types.field_description,
-                        all_fields=context.all_fields)
-                    types.validator.validate(item_context)
-                except ValidationException as exception:
-                    if context.all_fields:
-                        keypath_messages.update(exception.keypath_messages)
-                    else:
-                        raise exception
-            if len(keypath_messages) > 0:
-                raise ValidationException(keypath_messages=keypath_messages, root=context.root)
+        if types.field_description.item_nullability == Nullability.UNDEFINED:
+            types = types.required
+        keypath_messages = {}
+        for k, v in context.value.items():
+            try:
+                types.validator.validate(context.new(
+                    value=v,
+                    keypath=concat_keypath(context.keypath, k),
+                    keypath_owner=concat_keypath(context.keypath_owner, k),
+                    keypath_parent=k,
+                    parent=context.value))
+            except ValidationException as exception:
+                if context.all_fields:
+                    keypath_messages.update(exception.keypath_messages)
+                else:
+                    raise exception
+        if len(keypath_messages) > 0:
+            raise ValidationException(keypath_messages=keypath_messages, root=context.root)
 
     def transform(self, context: TransformingContext) -> Any:
         value = context.value
@@ -66,27 +58,16 @@ class DictOfValidator(TypeValidator):
         if not isinstance(value, dict):
             return value
         types = resolve_types(self.types, context.config.linked_class)
-        if types:
-            retval = {}
-            for k, v in value.items():
-                new_key = underscore(k) if context.config.camelize_json_keys else k
-                item_context = TransformingContext(
-                    value=v,
-                    keypath=concat_keypath(context.keypath, new_key),
-                    root=context.root,
-                    config=context.config,
-                    keypath_owner=concat_keypath(context.keypath_owner, new_key),
-                    owner=context.owner,
-                    config_owner=context.config_owner,
-                    keypath_parent=new_key,
-                    parent=value,
-                    field_description=types.field_description,
-                    all_fields=context.all_fields)
-                new_value = types.validator.transform(item_context)
-                retval[new_key] = new_value
-            return retval
-        else:
-            return value
+        retval = {}
+        for k, v in value.items():
+            new_key = underscore(k) if context.config.camelize_json_keys else k
+            retval[new_key] = types.validator.transform(context.new(
+                value=v,
+                keypath=concat_keypath(context.keypath, new_key),
+                keypath_owner=concat_keypath(context.keypath_owner, new_key),
+                keypath_parent=new_key,
+                parent=value))
+        return retval
 
     def tojson(self, context: ToJSONContext) -> Any:
         if context.value is None:
@@ -94,17 +75,9 @@ class DictOfValidator(TypeValidator):
         if type(context.value) is not dict:
             return context.value
         types = resolve_types(self.types, context.config.linked_class)
-        if types:
-            retval = {}
-            for k, v in context.value.items():
-                key = (camelize(k, False) if context.config.camelize_json_keys
-                       else k)
-                item_context = ToJSONContext(
-                    value=v,
-                    config=context.config,
-                    ignore_writeonly=context.ignore_writeonly)
-                val = types.validator.tojson(item_context)
-                retval[key] = val
-            return retval
-        else:
-            return context.value
+        retval = {}
+        for k, v in context.value.items():
+            key = (camelize(k, False) if context.config.camelize_json_keys
+                   else k)
+            retval[key] = types.validator.tojson(context.new(value=v))
+        return retval
