43be5ae19de23ea24a6f1537d11dc3fe62e026ad
Victor Zhang <yeannylam@gmail.com>
2020-10-05 19:35:33 +0800

    remove timestamp code

--- tests/test_orm_object.py
+++ tests/test_orm_object.py
@@ -3,7 +3,6 @@ from jsonclasses.exceptions import ValidationException
 from typing import List
 import unittest
 from jsonclasses import jsonclass, ORMObject, types
-from datetime import datetime
 
 
 class TestORMObject(unittest.TestCase):
@@ -174,65 +173,3 @@ class TestORMObject(unittest.TestCase):
         self.assertEqual(product.is_modified, False)
         self.assertEqual(product.modified_fields, set())
         self.assertRaisesRegex(ValidationException, "Value at 'user\\.name' should not be None\\.", product.validate)
-
-
-    # def test_persistable_json_object_has_created_at_on_initializing(self):
-    #     o = ORMObject()
-    #     self.assertTrue(type(o.created_at) is datetime)
-    #     self.assertTrue(o.created_at < datetime.now())
-
-    # def test_persistable_json_object_has_updated_at_on_initializing(self):
-    #     o = ORMObject()
-    #     self.assertTrue(type(o.updated_at) is datetime)
-    #     self.assertTrue(o.updated_at < datetime.now())
-
-    # def test_persistable_json_object_has_id_with_default_value_none(self):
-    #     o = ORMObject()
-    #     self.assertTrue(o.id is None)
-
-    # def test_persistable_json_object_id_can_be_int(self):
-    #     o = ORMObject()
-    #     o.id = 5
-    #     self.assertTrue(o.is_valid())
-
-    # def test_persistable_json_object_id_can_be_str(self):
-    #     o = ORMObject()
-    #     o.id = "sbd"
-    #     self.assertTrue(o.is_valid())
-
-    # def test_persistable_json_object_id_can_be_none(self):
-    #     o = ORMObject()
-    #     o.id = None
-    #     self.assertTrue(o.is_valid())
-
-    # def test_persistable_json_object_has_timestamps_in_nested_instances(self):
-    #     @jsonclass(graph='test_persistable_json_01')
-    #     class TestAuthor(ORMObject):
-    #         name: str
-    #         posts: List[TestPost] = types.listof('TestPost').linkedby('author')
-
-    #     @jsonclass(graph='test_persistable_json_01')
-    #     class TestPost(ORMObject):
-    #         title: str
-    #         content: str
-    #         author: TestAuthor = types.linkto.instanceof(TestAuthor)
-    #     input = {
-    #         'name': 'John Lesque',
-    #         'posts': [
-    #             {
-    #                 'title': 'Post One',
-    #                 'content': 'Great Article on Python.'
-    #             },
-    #             {
-    #                 'title': 'Post Two',
-    #                 'content': 'Great Article on JSON Classes.'
-    #             }
-    #         ]
-    #     }
-    #     author = TestAuthor(**input)
-    #     self.assertIs(type(author.created_at), datetime)
-    #     self.assertIs(type(author.updated_at), datetime)
-    #     self.assertIs(type(author.posts[0].created_at), datetime)
-    #     self.assertIs(type(author.posts[0].updated_at), datetime)
-    #     self.assertIs(type(author.posts[1].created_at), datetime)
-    #     self.assertIs(type(author.posts[1].updated_at), datetime)
