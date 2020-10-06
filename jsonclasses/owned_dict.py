9fc7b0e8aba2181635daa47d393bee9a8392e116
Victor Zhang <yeannylam@gmail.com>
2020-10-07 00:55:42 +0800

    owned collection concept

---
+++ jsonclasses/owned_dict.py
@@ -0,0 +1,8 @@
+"""The owner observable dict."""
+
+
+class OwnedDict(dict):
+    pass
+
+# owner: dict did add object at key
+# owner: dict did remove object at key
---
+++ jsonclasses/owned_list.py
@@ -0,0 +1,8 @@
+"""The owner observable list."""
+
+
+class OwnedList(list):
+    pass
+
+# owner: list did add object at index
+# owner: list did remove object at index
