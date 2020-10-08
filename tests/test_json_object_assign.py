cf21ccc54c7a75ed748204caf354a5f881ab8bda
Victor Zhang <yeannylam@gmail.com>
2020-10-08 23:30:28 +0800

    relationship connects on assign

--- tests/test_json_object_assign.py
+++ tests/test_json_object_assign.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import List
 from unittest import TestCase
 from jsonclasses import jsonclass, JSONObject, types
 
@@ -17,11 +18,58 @@ class User(JSONObject):
     staff: Staff = types.instanceof('Staff').linkedby('user').required
 
 
+@jsonclass(graph='test_json_object_assign')
+class Post(JSONObject):
+    id: int
+    name: str
+    author: Author = types.linkto.instanceof('Author').required
+
+
+@jsonclass(graph='test_json_object_assign')
+class Author(JSONObject):
+    id: int
+    name: str
+    posts: List[Post] = types.listof('Post').linkedby('author').required
+
+
 class TestJSONObjectAssign(TestCase):
 
-    def test_json_objects_connects_thru_assign(self):
+    def test_json_objects_connects_1k_1l_thru_assign(self):
         staff = Staff(id=1, position='CEO')
         user = User(id=1, name='Victor')
         user.staff = staff
         self.assertEqual(user.staff.user, user)
         self.assertEqual(staff.user.staff, staff)
+
+    def test_json_objects_connects_1l_1k_thru_assign(self):
+        staff = Staff(id=1, position='CEO')
+        user = User(id=1, name='Victor')
+        staff.user = user
+        self.assertEqual(user.staff.user, user)
+        self.assertEqual(staff.user.staff, staff)
+
+    def test_json_objects_connects_1_many_empty_thru_assign(self):
+        post = Post(id=1, name='Zayton City')
+        author = Author(id=1, name='Victor')
+        post.author = author
+        self.assertEqual(len(author.posts), 1)
+        self.assertEqual(post.author.posts[0], post)
+        self.assertEqual(author.posts[0].author, author)
+        post.author = author
+        self.assertEqual(len(author.posts), 1)
+        self.assertEqual(post.author.posts[0], post)
+        self.assertEqual(author.posts[0].author, author)
+
+    def test_json_objects_connects_1_many_one_exist_thru_assign(self):
+        post = Post(id=1, name='Zayton City')
+        author = Author(id=1, name='Victor')
+        post.author = author
+        post2 = Post(id=2, name='Chinkang City')
+        post2.author = author
+        self.assertEqual(len(author.posts), 2)
+        self.assertEqual(post.author.posts[1], post2)
+        self.assertEqual(author.posts[1].author, author)
+        post2.author = author
+        self.assertEqual(len(author.posts), 2)
+        self.assertEqual(post.author.posts[1], post2)
+        self.assertEqual(author.posts[1].author, author)
