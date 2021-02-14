88ac8aa665887e04fcfb0dad04e71af875946496
Victor Zhang <yeannylam@gmail.com>
2021-02-14 20:41:42 +0800

    abstract class tests

--- tests/test_abstract_class.py
+++ tests/test_abstract_class.py
@@ -24,3 +24,23 @@ class TestAbstractClass(TestCase):
             created_at: datetime
             updated_at: datetime
         MyORMObject()
+
+    def test_abstract_classs_non_abstract_subclass_can_be_initialized(self):
+        @jsonclass(class_graph='test_abstract_class_3', abstract=True)
+        class MyORMObject(JSONObject):
+            id: str
+            created_at: datetime
+            updated_at: datetime
+
+        @jsonclass(class_graph='test_abstract_class_3')
+        class MyObject(MyORMObject):
+            name: str
+        MyObject()
+
+    def test_abstract_class_class_is_not_abstract_by_default(self):
+        @jsonclass(class_graph='test_abstract_class_4')
+        class MyORMObject(JSONObject):
+            id: str
+            created_at: datetime
+            updated_at: datetime
+        MyORMObject()
