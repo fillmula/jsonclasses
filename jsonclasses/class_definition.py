04b6eb38e2cc16cf39ec53c11af15d994a4c406b
Victor Zhang <yeannylam@gmail.com>
2021-02-21 21:15:45 +0800

    class definition

--- jsonclasses/class_definition.py
+++ jsonclasses/class_definition.py
@@ -5,13 +5,14 @@ user defines a JSON class. This is used by the framework to lookup class fields
 and class field settings.
 """
 from __future__ import annotations
-from typing import final, TYPE_CHECKING
+from typing import Optional, final, TYPE_CHECKING
 from dataclasses import fields, Field
 from inflection import camelize
 from .jsonclass_field import JSONClassField
 if TYPE_CHECKING:
     from .new_config import Config
     from .types import Types
+    from .new_types_resolver import TypesResolver
 
 
 @final
@@ -31,8 +32,12 @@ class ClassDefinition:
                 created.
             config (Config): The configuration object for the targeted class.
         """
-        self._list_fields = []
-        self._dict_fields = {}
+        self._list_fields: list[JSONClassField] = []
+        self._dict_fields: dict[str, JSONClassField] = {}
+        self._primary_field: Optional[JSONClassField] = None
+        self._created_at_field: Optional[JSONClassField] = None
+        self._updated_at_field: Optional[JSONClassField] = None
+        self._deleted_at_field: Optional[JSONClassField] = None
         for field in fields(class_):
             name = field.name
             if config.camelize_json_keys:
@@ -44,19 +49,31 @@ class ClassDefinition:
             else:
                 db_name = name
             types = self._get_types(field, config.linked_class)
-            assigned_default_value = None if isinstance(field.default, Types) else field.default
-            if field.default == field.default_factory:  # type: ignore
-                assigned_default_value = None
+            if isinstance(field.default, Types):
+                default = None
+            elif field.default == field.default_factory:
+                default = None
+            else:
+                default = field.default
             jsonclass_field = JSONClassField(
                 name=name,
-                json_name==json_name,
-                db_name==db_name,
-                field_types=field_types,
-                assigned_default_value=assigned_default_value,
-                fdesc=field_types.fdesc,
-                field_validator=field_types.validator)
+                json_name=json_name,
+                db_name=db_name,
+                default=default,
+                types=types,
+                definition=types.fdesc,
+                validator=types.validator)
             self._list_fields.append(jsonclass_field)
             self._dict_fields[name] = jsonclass_field
+            types.fdesc
+            if types.fdesc.primary:
+                self._primary_field = jsonclass_field
+            if types.fdesc.usage == 'created_at':
+                self._created_at_field = jsonclass_field
+            elif types.fdesc.usage == 'updated_at':
+                self._updated_at_field = jsonclass_field
+            elif types.fdesc.usage == 'deleted_at':
+                self._deleted_at_field = jsonclass_field
 
     def _get_types(self: ClassDefinition,
                    field: Field,
@@ -70,4 +87,56 @@ class ClassDefinition:
         if isinstance(field.default, Types):
             return field.default
         else:
-            return to_types(field.type, graph_sibling)
+            return TypesResolver().to_types(field.type, config)
+
+    def field_named(self: ClassDefinition, name: str) -> JSONClassField:
+        """
+        Get the field which is named `name`.
+
+        Args:
+            name (str): The name of the field to return.
+
+        Returns:
+            JSONClassField: The field named `name`.
+
+        Raises:
+            ValueError: If can't find a field with name `name`.
+        """
+        if not self._dict_fields.get(name):
+            raise ValueError(f'no field named {name} in class definition')
+        return self._dict_fields[name]
+
+    @property
+    def created_at_field(self: ClassDefinition) -> Optional[JSONClassField]:
+        """
+        The class definition's field which represents the created at field.
+
+        This is used by the framework to locate the correct field to find the
+        record's created at timestamp.
+        """
+        return self._created_at_field
+
+    @property
+    def updated_at(self: ClassDefinition) -> Optional[JSONClassField]:
+        """The class definition's field which represents the updated at field.
+
+        This is used by the framework to locate the correct field to find the
+        record's updated at timestamp.
+        """
+        return self._updated_at_field
+
+    @property
+    def deleted_at_field(self: ClassDefinition) -> Optional[JSONClassField]:
+        """The class definition's field which represents the deleted at field.
+
+        This is used by the framework to locate the correct field to find the
+        record's deleted at timestamp.
+        """
+        return self._deleted_at_field
+
+    @property
+    def primary_field(self: ClassDefinition) -> Optional[JSONClassField]:
+        """The class definition's primary field. This can be None if it's not
+        defined by user.
+        """
+        return self._primary_field
