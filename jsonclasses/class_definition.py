665d395ab5d970a5ff8648c8fbd3c0740d99e9f9
Victor Zhang <yeannylam@gmail.com>
2021-02-22 11:18:24 +0800

    class definition

--- jsonclasses/class_definition.py
+++ jsonclasses/class_definition.py
@@ -5,14 +5,15 @@ user defines a JSON class. This is used by the framework to lookup class fields
 and class field settings.
 """
 from __future__ import annotations
-from typing import Optional, final, TYPE_CHECKING
+from typing import Optional, final, cast, TYPE_CHECKING
 from dataclasses import fields, Field
 from inflection import camelize
 from .jsonclass_field import JSONClassField
+from .fields import FieldStorage, FieldType
+from .new_types_resolver import TypesResolver
 if TYPE_CHECKING:
     from .new_config import Config
     from .types import Types
-    from .new_types_resolver import TypesResolver
 
 
 @final
@@ -32,8 +33,11 @@ class ClassDefinition:
                 created.
             config (Config): The configuration object for the targeted class.
         """
+        self._class_name: str = class_.__name__
+        self._config: Config = config
         self._list_fields: list[JSONClassField] = []
         self._dict_fields: dict[str, JSONClassField] = {}
+        self._foreign_fields: dict[str, tuple[ClassDefinition, str]] = {}
         self._primary_field: Optional[JSONClassField] = None
         self._created_at_field: Optional[JSONClassField] = None
         self._updated_at_field: Optional[JSONClassField] = None
@@ -89,6 +93,20 @@ class ClassDefinition:
         else:
             return TypesResolver().to_types(field.type, config)
 
+    @property
+    def class_name(self: ClassDefinition) -> str:
+        """The name of the JSON class on which this class definition is
+        defined.
+        """
+        return self._class_name
+
+    @property
+    def config(self: ClassDefinition) -> Config:
+        """The configuration object of the JSON class on which this class
+        definition is defined.
+        """
+        return self._config
+
     def field_named(self: ClassDefinition, name: str) -> JSONClassField:
         """
         Get the field which is named `name`.
@@ -140,3 +158,26 @@ class ClassDefinition:
         defined by user.
         """
         return self._primary_field
+
+    @property
+    def foreign_field_for(self: ClassDefinition,
+                          name: str) -> Optional[tuple[ClassDefinition, str]]:
+        """
+        """
+        if self._foreign_fields.get(name):
+            return self._foreign_fields.get(name)
+        local_field = self.field_named(name)
+        definition = local_field.definition
+        resolver = TypesResolver()
+        if definition.field_storage not in \
+                [FieldStorage.LOCAL_KEY, FieldStorage.FOREIGN_KEY]:
+            raise ValueError(f"field named '{name}' is not a linked field")
+        if definition.field_type == FieldType.INSTANCE:
+            foreign_types = resolver.resolve_types(
+                definition.instance_types, self.config)
+        elif definition.field_type == FieldType.LIST:
+            instance_types = resolver.resolve_types(
+                definition.raw_item_types, self.config)
+            foreign_types = resolver.resolve_types(
+                instance_types, self.config)
+        foreign_class = cast(type, foreign_types.fdesc.instance_types)
