98183717092920a1ebda3b3ff389968c06e52404
Victor Zhang <yeannylam@gmail.com>
2021-05-08 15:07:40 +0800

    modified fields test for local key

--- tests/test_local_key.py
+++ tests/test_local_key.py
@@ -14,6 +14,15 @@ class TestLocalKey(TestCase):
         article = LKArticle(name='A', author=author)
         self.assertEqual(article.author_id, author.id)
 
+    def test_set_local_key_resets_object(self):
+        author = LKAuthor(name='A')
+        article = LKArticle(name='A', author=author)
+        self.assertEqual(article.author, author)
+        self.assertEqual(author.articles[0], article)
+        article.author_id = 500
+        self.assertEqual(article.author, None)
+        self.assertEqual(author.articles, [])
+
     def test_local_key_is_set_to_none_then_object_is_set_to_none(self):
         author = LKAuthor(name='A')
         article = LKArticle(name='A', author=author)
@@ -40,3 +49,21 @@ class TestLocalKey(TestCase):
         author.articles = []
         self.assertEqual(article.author_id, None)
         self.assertEqual(author.articles, [])
+
+    def test_local_key_is_considered_modified_field(self):
+        author = LKAuthor(name='A')
+        article = LKArticle(name='A', author=author)
+        setattr(author, '_is_new', False)
+        setattr(article, '_is_new', False)
+        article.author = None
+        self.assertEqual(article.modified_fields, ('author',))
+        self.assertEqual(author.modified_fields, ('articles',))
+
+    def test_local_key_is_considered_modified_field_with_id_assign(self):
+        author = LKAuthor(name='A')
+        article = LKArticle(name='A', author=author)
+        setattr(author, '_is_new', False)
+        setattr(article, '_is_new', False)
+        article.author_id = None
+        self.assertEqual(article.modified_fields, ('author',))
+        self.assertEqual(author.modified_fields, ('articles',))
