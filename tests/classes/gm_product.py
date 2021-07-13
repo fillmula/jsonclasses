8703eded39a57f4f30f99710adf3d3b10be4db30
Victor Teo <yeannylam@gmail.com>
2021-07-13 17:37:39 +0800

    test can delete

---
+++ tests/classes/gm_product.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from jsonclasses import jsonclass, types
+
+
+def check_owner(product: GMProduct, operator: GMProductUser) -> bool:
+    return product.user.id == operator.id
+
+
+def check_tier(product: GMProduct, operator: GMProductUser) -> bool:
+    return operator.paid_user
+
+
+@jsonclass
+class GMProductUser:
+    id: str
+    name: str
+    paid_user: bool
+    books: list[GMProduct] = types.nonnull.listof('GMProduct').linkedby('user')
+
+
+@jsonclass(can_delete=[check_owner, check_tier])
+class GMProduct:
+    name: str
+    user: GMProductUser = types.instanceof('GMProductUser').linkto.required
---
+++ tests/classes/gs_product.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from jsonclasses import jsonclass, types
+
+
+def check_user(product: GSProduct, operator: GSProductUser) -> bool:
+    return product.user.id == operator.id and operator.paid_user
+
+
+@jsonclass
+class GSProductUser:
+    id: str
+    name: str
+    paid_user: bool
+    books: list[GSProduct] = types.nonnull.listof('GSProduct').linkedby('user')
+
+
+@jsonclass(can_delete=check_user)
+class GSProduct:
+    name: str
+    user: GSProductUser = types.instanceof('GSProductUser').linkto.required
