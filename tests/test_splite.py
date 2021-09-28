8fa773351c8d14581e75cfaf7c24832ec0d8ad62
Ben Wong <ben.wong@fillmula.com>
2021-09-28 17:57:51 +0800

    split

--- tests/test_splite.py
+++ tests/test_splite.py
@@ -5,10 +5,10 @@ from unittest import TestCase
 
 class TestSplit(TestCase):
 
-    def test_split_keeps_list_value_if_its_list_str(self):
+    def test_split_keeps_list_value_if_it_is_list_of_strs(self):
         s = SuperIterable(itssp=["a", "d", "s", "r"])
         self.assertEqual(s.itssp, ["a", "d", "s", "r"])
 
-    def test_split_transforms_if_its_str(self):
+    def test_split_splits_str_into_a_list_of_substrs(self):
         s = SuperIterable(itssp="abc.ous.fga.ssr")
         self.assertEqual(s.itssp, ["abc", "ous", "fga", "ssr"])
