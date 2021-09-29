7c51a5a850bf2bd7568a9b98ca8ad0da85883f87
Victor Teo <victor.teo@fillmula.com>
2021-09-29 12:55:25 +0800

    fix test message

--- tests/test_tobool.py
+++ tests/test_tobool.py
@@ -5,12 +5,10 @@ from tests.classes.super_type import SuperType
 
 class TestToBool(TestCase):
 
-    def test_tobool_transforms_true__if_its_value_is_to_true(self):
+    def test_tobool_returns_true_if_value_evaluates_to_true(self):
         b = SuperType(tb={1, 3, 4})
         self.assertEqual(b.tb, True)
 
-    def test_tobool_transforms_false_if_its_value_is_to_false(self):
+    def test_tobool_returns_false_if_value_evaluates_to_false(self):
         b = SuperType(tb="")
         self.assertEqual(b.tb, False)
-
-
