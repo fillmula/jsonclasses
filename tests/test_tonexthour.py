8c634473fb0e373a9b65ab2b5f675e6cc8ef08f6
Snoopy Yung <snoopy.yung@fillmula.com>
2021-10-12 14:25:44 +0800

    fix test method name with tobosec tobomin tobohour tonextsec tonextmin and tonexhour

--- tests/test_tonexthour.py
+++ tests/test_tonexthour.py
@@ -5,10 +5,10 @@ from tests.classes.super_datetime import SuperDateTime
 
 class TestTonexthour(TestCase):
 
-    def test_time_of_next_minute_value_is_datetime(self):
+    def test_tonexthour_time_of_next_minute_value_is_datetime(self):
         d = SuperDateTime(dtnh=datetime(2021, 10, 11, 17, 37, 27, 446259))
         self.assertEqual(d.dtnh, datetime(2021,10, 11, 18, 0))
 
-    def test_time_of_next_minute_value_is_not_datetime(self):
+    def test_tonexthour_time_of_next_minute_value_is_not_datetime(self):
         s = SuperDateTime(stnh="12345")
         self.assertEqual(s.stnh, "12345")
