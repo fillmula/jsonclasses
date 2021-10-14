197951683d780173559d0afdda5d39c279e5adcb
Snoopy Yung <snoopy.yung@fillmula.com>
2021-10-14 10:26:54 +0800

    fix test eq

--- tests/test_eq.py
+++ tests/test_eq.py
@@ -7,11 +7,11 @@ from unittest import TestCase
 class TestEq(TestCase):
 
     def test_eq_does_not_raise_if_equal(self):
-        e = SimpleEq(neq_value="dsadsa", eq_value="dsadsa")
+        e = SimpleEq(eq_value="dsadsa", neq_value="dssa")
         e.validate()
 
     def test_eq_raises_if_is_not_equal(self):
-        e = SimpleEq(neq_value="dsa", eq_value="dsadsa")
+        e = SimpleEq(eq_value="dsa", neq_value="dsadsa")
         with self.assertRaises(ValidationException) as context:
             e.validate()
         self.assertEqual(context.exception.keypath_messages['eqValue'],
