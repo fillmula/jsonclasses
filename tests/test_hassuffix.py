924033ddcc63c8a1d97d800ebe0e62e8aab6615d
Snoopy Yung <snoopy.yung@fillmula.com>
2021-10-18 01:14:23 +0800

    fix hasprefix,hassuffix,etc test name

--- tests/test_hassuffix.py
+++ tests/test_hassuffix.py
@@ -6,22 +6,22 @@ from unittest import TestCase
 
 class TestHasSuffix(TestCase):
 
-    def test_hassuffix_validates_original_str_is_suffix_of_a_str(self):
+    def test_hassuffix_validates_str_val_is_suffix_of_str_param(self):
         s_hs = SuperSuffix(s_hs='this is python')
         s_hs.validate()
 
-    def test_hassuffix_validates_original_list_is_suffix_of_a_list(self):
+    def test_hassuffix_validates_list_val_is_suffix_of_list_param(self):
         l_hs = SuperSuffix(l_hs=['qq', 'dd', 'ee', 'ff'])
         l_hs.validate()
 
-    def test_hassuffix_raises_if_original_str_is_not_suffix_of_a_str(self):
+    def test_hassuffix_raises_if_str_val_is_not_suffix_of_str_param(self):
         s_hs = SuperSuffix(s_hs='This ')
         with self.assertRaises(ValidationException) as context:
             s_hs.validate()
         self.assertEqual(context.exception.keypath_messages['sHs'],
                          "suffix is not found")
 
-    def test_hassuffix_raises_if_original_list_is_not_suffix_of_a_list(self):
+    def test_hassuffix_raises_if_list_val_is_not_suffix_of_list_param(self):
         l_hs = SuperSuffix(l_hs=['qq'])
         with self.assertRaises(ValidationException) as context:
             l_hs.validate()
