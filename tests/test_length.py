0b7517a4d38433c34de8d815a64c3615b421dfcc
Victor Teo <victor.teo@fillmula.com>
2021-10-18 00:06:29 +0800

    update length test case names

--- tests/test_length.py
+++ tests/test_length.py
@@ -7,7 +7,7 @@ from tests.classes.simple_code import SimpleCode
 
 class TestLength(TestCase):
 
-    def test_length_does_not_raise_length_in_between(self):
+    def test_length_does_not_raise_if_length_is_in_between(self):
         c1 = SimpleCompany(name='AZ', number_of_employees=5)
         c1.validate()
         c2 = SimpleCompany(name='QWERT', number_of_employees=5)
@@ -15,25 +15,25 @@ class TestLength(TestCase):
         c3 = SimpleCompany(name='QWERTQWERT', number_of_employees=5)
         c3.validate()
 
-    def test_length_raises_length_lt_lower(self):
+    def test_length_raises_if_length_is_lt_lowerbond(self):
         c1 = SimpleCompany(name='Q', number_of_employees=5)
         with self.assertRaises(ValidationException):
             c1.validate()
 
-    def test_length_raises_length_gt_lower(self):
+    def test_length_raises_if_length_is_gt_lowerbond(self):
         c1 = SimpleCompany(name='QWERTYUIOP{', number_of_employees=5)
         with self.assertRaises(ValidationException):
             c1.validate()
 
-    def test_length_should_be_match_in_str(self):
+    def test_length_should_match_length_in_str(self):
         code = SimpleCode(code='1234')
         code.validate()
 
-    def test_length_should_be_match_in_list(self):
+    def test_length_should_match_length_in_list(self):
         l_code = SimpleCode(l_code=[1, 2, 3, 4])
         l_code.validate()
 
-    def test_length_raises_if_length_not_match_in_str(self):
+    def test_length_raises_if_length_does_not_match_in_str(self):
         code = SimpleCode(code='12345')
         with self.assertRaises(ValidationException):
             code.validate()
@@ -41,7 +41,7 @@ class TestLength(TestCase):
         with self.assertRaises(ValidationException):
             code.validate()
 
-    def test_length_raises_if_length_not_match_in_list(self):
+    def test_length_raises_if_length_does_not_match_in_list(self):
         l_code = SimpleCode(l_code=[1, 2, 3, 4, 5, 0])
         with self.assertRaises(ValidationException):
             l_code.validate()
