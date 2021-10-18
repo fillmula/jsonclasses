c9604412d636bce5ad446701061efdfdf0aefbd9
Victor Teo <victor.teo@fillmula.com>
2021-10-18 17:55:19 +0800

    update neq

--- jsonclasses/modifiers/neq_modifier.py
+++ jsonclasses/modifiers/neq_modifier.py
@@ -14,9 +14,5 @@ class NeqModifier(Modifier):
         self.val = val
 
     def validate(self, ctx: Ctx) -> None:
-        if ctx.val is None:
-            return
         if ctx.val == self.resolve_param(self.val, ctx):
             ctx.raise_vexc('value is not unequal')
-        else:
-            return ctx.val
