fdef25cf27c49be616c71cc49d4db993b530fa01
Snoopy Yung <snoopy.yung@fillmula.com>
2021-10-19 11:21:00 +0800

    delete ds store

--- jsonclasses/modifiers/issuffixof_modifier.py
+++ jsonclasses/modifiers/issuffixof_modifier.py
@@ -10,12 +10,12 @@ if TYPE_CHECKING:
 class IsSuffixOfModifier(Modifier):
     """Has suffix modifier for checking if a str is suffix of another str."""
 
-    def __init__(self, suffix: str | list[Any] | Callable | Types) -> None:
-        self.suffix = suffix
+    def __init__(self, is_suffix: str | list[Any] | Callable | Types) -> None:
+        self.is_suffix = is_suffix
 
     def validate(self, ctx: Ctx) -> None:
         if isinstance(ctx.val, list):
-            if not self.resolve_param(self.suffix, ctx)[len(ctx.val):] == ctx.val:
+            if not self.resolve_param(self.is_suffix, ctx)[-len(ctx.val):] == ctx.val:
                 ctx.raise_vexc('suffix is not found')
         elif type(ctx.val) is str:
             if not self.resolve_param(self.suffix, ctx).endswith(ctx.val):
