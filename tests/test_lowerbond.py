6fd59da4625885271a320ea36f91b03dc913fde5
Snoopy Yung <snoopy.yung@fillmula.com>
2021-10-20 17:45:05 +0800

    fix upperbond, lowerbond and test name

--- tests/test_lowerbond.py
+++ tests/test_lowerbond.py
@@ -5,34 +5,34 @@ from tests.classes.super_bond import SuperBond
 
 class TestLowerBond(TestCase):
 
-    def test_lower_bond_transforms_int_param_if_it_less_than_int_val(self):
+    def test_lowerbond_transforms_int_param_if_it_is_less_than_int_val(self):
         n = SuperBond(i_lb=-1)
         self.assertEqual(n.i_lb, 1)
 
-    def test_lower_bond_transforms_float_param_if_it_less_than_float_val(self):
+    def test_lowerbond_transforms_float_param_if_it_is_less_than_float_val(self):
         n = SuperBond(f_lb=-1.0)
         self.assertEqual(n.f_lb, 1.0)
 
-    def test_lower_bond_transforms_callable_param_if_it_less_than_callable_val(self):
+    def test_lowerbond_transforms_callable_param_if_it_is_less_than_callable_val(self):
         n = SuperBond(c_lb=-1)
         self.assertEqual(n.c_lb, 1)
 
-    def test_lower_bond_transforms_types_param_if_it_less_than_types_val(self):
+    def test_lowerbond_transforms_types_param_if_it_is_less_than_types_val(self):
         n = SuperBond(t_lb=-1)
         self.assertEqual(n.t_lb, 1)
 
-    def test_lower_bond_transforms_int_param_if_it_not_less_than_int_val(self):
+    def test_lowerbond_does_not_transform_int_param_if_it_is_greater_than_int_val(self):
         n = SuperBond(i_lb=2)
         self.assertEqual(n.i_lb, 2)
 
-    def test_lower_bond_transforms_float_param_if_it_not_less_than_float_val(self):
+    def test_lowerbond_does_not_transform_float_param_if_it_is_greater_than_float_val(self):
         n = SuperBond(f_lb=2.0)
         self.assertEqual(n.f_lb, 2.0)
 
-    def test_lower_bond_transforms_callable_param_if_it_not_less_than_callable_val(self):
+    def test_lowerbond_does_not_transform_callable_param_if_it_is_greater_than_callable_val(self):
         n = SuperBond(c_lb=2)
         self.assertEqual(n.c_lb, 2)
 
-    def test_lower_bond_transforms_types_param_if_it_not_less_than_types_val(self):
+    def test_lowerbond_does_not_transform_types_param_if_it_is_greater_than_types_val(self):
         n = SuperBond(t_lb=2)
         self.assertEqual(n.t_lb, 2)
--- tests/test_upperbond.py
+++ tests/test_upperbond.py
@@ -5,34 +5,34 @@ from tests.classes.super_bond import SuperBond
 
 class TestUpperBond(TestCase):
 
-    def test_upper_bond_transforms_int_param_if_it_larger_than_int_val(self):
+    def test_upperbond_transforms_int_param_if_it_is_larger_than_int_val(self):
         n = SuperBond(i_ub=151)
         self.assertEqual(n.i_ub, 150)
 
-    def test_upper_bond_transforms_float_param_if_it_larger_than_float_val(self):
+    def test_upperbond_transforms_float_param_if_it_is_larger_than_float_val(self):
         n = SuperBond(f_ub=151.0)
         self.assertEqual(n.f_ub, 150.0)
 
-    def test_upper_bond_transforms_callable_param_if_it_larger_than_callable_val(self):
+    def test_upperbond_transforms_callable_param_if_it_is_larger_than_callable_val(self):
         n = SuperBond(c_ub=151)
         self.assertEqual(n.c_ub, 150)
 
-    def test_upper_bond_transforms_types_param_if_it_larger_than_types_val(self):
+    def test_upperbond_transforms_types_param_if_it_is_larger_than_types_val(self):
         n = SuperBond(t_ub=151)
         self.assertEqual(n.t_ub, 150)
 
-    def test_upper_bond_transforms_int_param_if_it_not_larger_than_int_val(self):
+    def test_upperbond_does_not_transform_int_param_if_it_is_less_than_int_val(self):
         n = SuperBond(i_ub=100)
         self.assertEqual(n.i_ub, 100)
 
-    def test_upper_bond_transforms_float_param_if_it_not_larger_than_float_val(self):
+    def test_upperbond_does_not_transform_float_param_if_it_is_less_than_float_val(self):
         n = SuperBond(f_ub=100.0)
         self.assertEqual(n.f_ub, 100.0)
 
-    def test_upper_bond_transforms_callable_param_if_it_not_larger_than_callable_val(self):
+    def test_upperbond_does_not_transform_callable_param_if_it_is_less_than_callable_val(self):
         n = SuperBond(c_ub=100)
         self.assertEqual(n.c_ub, 100)
 
-    def test_upper_bond_transforms_types_param_if_it_not_larger_than_types_val(self):
+    def test_upperbond_does_not_transform_types_param_if_it_is_less_than_types_val(self):
         n = SuperBond(t_ub=100)
         self.assertEqual(n.t_ub, 100)
