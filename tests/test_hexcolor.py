f7d69ef3349ee9b6a0cf567b775898a31c35f8f4
Victor Teo <victor.teo@fillmula.com>
2021-11-11 11:28:52 +0800

    add transform test for hexcolor

--- tests/test_hexcolor.py
+++ tests/test_hexcolor.py
@@ -6,17 +6,18 @@ from tests.classes.hex_color import HexColor
 
 class TestHexColor(TestCase):
 
-    def test_hex_color_doesnt_raise(self):
+    def test_hexcolor_removes_initial_pound(self):
+        color = HexColor(hex_color='#01FD6F')
+        self.assertEqual(color.hex_color, '01FD6F')
+
+    def test_hexcolor_doesnt_raise(self):
         color = HexColor(hex_color='01FD6F')
         color.validate()
 
-    def test_hex_color_raises_if_value_is_not_valid(self):
+    def test_hexcolor_raises_if_value_is_not_valid(self):
         color = HexColor(hex_color='ZZZZZZ')
         with self.assertRaises(ValidationException) as context:
             color.validate()
         self.assertEqual(len(context.exception.keypath_messages), 1)
         self.assertEqual(context.exception.keypath_messages['hexColor'],
                          "value is not hex color string")
-    
-    
-
